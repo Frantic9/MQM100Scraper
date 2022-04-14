c50d82cadb73f41011b3b72a1a409d994c4e2a63
FranticNine <franticnine2015@gmail.com>
2022-04-14 11:20:35 -0500

    Refactored Functions

--- LinkGrabber.py
+++ LinkGrabber.py
@@ -1,44 +1,35 @@
-import requests
 from bs4 import BeautifulSoup
 import time
 from selenium import webdriver
-
-f = open("Links.txt", "a")
-print("How many Links you want?")
-size = input()
-len = int(size)
-ref_array = []
-
-
 
 def render_page(url):
     path = "C:\Program Files (x86)\chromedriver.exe"
     driver = webdriver.Chrome(path)
     driver.get(url)
-    time.sleep(1)
+    time.sleep(1.5)
     r = driver.page_source
     return r
 
-
-for x in range(len):
+def get_html():
     goog_search = "https://randomrepo.com/"
-
     r  = render_page(goog_search)
-
     soup = BeautifulSoup(r, "html.parser")
     soup = soup.find("div", {"class": "col-12 text-center"}).find("a")
-    ref = soup.get("href")
-    ref_array.append(ref)
+    return soup.get("href")
+
+f = open("Links.txt", "a")
+print("How many Links would you like?")
+size = input()
+len = int(size)
+ref_array = []
+
+for x in range(len):
+    ref_array.append(get_html())
 
 for x in range(len):
     for j in range(x+1,len):
         if(ref_array[x] == ref_array[j]):
-            goog_search = "https://randomrepo.com/"
-            r  = render_page(goog_search)
-            soup = BeautifulSoup(r, "html.parser")
-            soup = soup.find("div", {"class": "col-12 text-center"}).find("a")
-            ref = soup.get("href")
-            ref_array[j] = ref
+            ref_array[j] = get_html()
 
 
 for x in range(len):
